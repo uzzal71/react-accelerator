a39b33a836f8fafa947b45bbb33da46b49589966
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-12 00:11:49 +0600

    version 1.7

--- 1.6/src/App.jsx
+++ 1.6/src/App.jsx
@@ -1,11 +1,9 @@
-import Test from "./components/Test";
+const today = new Date();
 
-function App() {
-  return (
-    <div>
-      <Test />
-    </div>
-  );
+function formatDate(date) {
+  return new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date);
 }
 
-export default App;
+export default function App() {
+  return <h1>To Do List for {formatDate(today)}</h1>;
+}
