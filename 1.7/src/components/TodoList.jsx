bf2236e27e11c6dacccb526cf4bd5c65ae621f17
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-12 00:27:23 +0600

    version 1.7

--- 1.7/src/components/TodoList.jsx
+++ 1.7/src/components/TodoList.jsx
@@ -9,8 +9,15 @@ export default function TodoList() {
 
   const color = "green";
 
+  let style = "";
+  if (color === "green") {
+    style = style1;
+  } else {
+    style = style2;
+  }
+
   return (
-    <ul style={color === "green" ? style1 : style2}>
+    <ul style={style}>
       <li>Improve the videophone</li>
       <li>Prepare aeronautics lectures</li>
       <li>Work on the alcohol-fuelled engine</li>
