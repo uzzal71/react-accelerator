e9f8910c1f5c35ffbaaed68603834aa90d6cc5b6
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-13 02:29:24 +0600

    version 1.9

--- 1.9/src/components/Item.jsx
+++ 1.9/src/components/Item.jsx
@@ -1,6 +1,7 @@
 export default function Item({ name, isPacked }) {
-  if (isPacked) {
-    return <li className="item">{name} ✔</li>;
-  }
-  return <li className="item">{name}</li>;
+  return (
+    <li className="item">
+      {name} {isPacked && "✔"}
+    </li>
+  );
 }
--- 1.9/src/components/PackingList.jsx
+++ 1.9/src/components/PackingList.jsx
@@ -12,3 +12,12 @@ export default function PackingList() {
     </section>
   );
 }
+
+// just for memory
+const lang = "JavaScript1";
+const result1 = lang && "JavaScript"; // left side truthly hole right side return korbe, other wise left side return korbe
+const result2 = lang || "JavaScript"; // left side falsy hole right side return korbe, other wise left side return korbe
+const result3 = lang ?? "JavaScript"; // nullish coalesing operator: left side nullish(null or undefined) hole right side return korbe, otherwise left side return korbe
+console.log(result1); // JavaScript
+console.log(result2); // JavaScript1
+console.log(result3); // JavaScript1
