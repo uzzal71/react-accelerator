68ffc171b6d6a41cbc06048e5be4e69d446a2af9
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-13 16:25:57 +0600

    Tic-Tac-Toe

--- 1.13/src/App.jsx
+++ 1.13/src/App.jsx
@@ -1,3 +1,44 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+/**
+  Game,
+    -> Board
+      -> Square
+    -> History
+*/
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button
+      type="button"
+      className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg"
+      onClick={onSquareClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  const handleClick = (i) => {
+    alert(i);
+  };
+
+  return (
+    <>
+      <div>
+        <Square value="1" onSquareClick={() => handleClick(0)} />
+        <Square value="2" onSquareClick={() => handleClick(1)} />
+        <Square value="3" onSquareClick={() => handleClick(2)} />
+      </div>
+      <div>
+        <Square value="4" onSquareClick={() => handleClick(3)} />
+        <Square value="5" onSquareClick={() => handleClick(4)} />
+        <Square value="6" onSquareClick={() => handleClick(5)} />
+      </div>
+      <div>
+        <Square value="7" onSquareClick={() => handleClick(6)} />
+        <Square value="8" onSquareClick={() => handleClick(7)} />
+        <Square value="9" onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
 }
---
+++ 1.13/src/components/Square.jsx
@@ -0,0 +1,3 @@
+export default function Square() {
+  return <button type="button">X</button>;
+}
