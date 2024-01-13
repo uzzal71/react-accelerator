fe0e4a8ae5f4ca76c31c27269d8a7d5f371f09b4
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-13 18:45:06 +0600

    Tic-Tac-Toe

--- 1.13/src/App.jsx
+++ 1.13/src/App.jsx
@@ -67,13 +67,35 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [xIsNext, setXIsNext] = useState(true);
+  const [currentMove, setCurrentMove] = useState(0);
 
-  const currentSquares = history[history.length - 1];
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
     setXIsNext(!xIsNext);
-    setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
+
+  function jumpTo(move) {
+    setCurrentMove(move);
+    setXIsNext(move % 2 === 0);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = `Go to the move # ${move}`;
+    } else {
+      description = `Go to start the game`;
+    }
+    return (
+      <li key={move} className="bg-gray-700 text-white mb-1 p-1 rounded-sm">
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   return (
     <div>
@@ -81,7 +103,7 @@ export default function Game() {
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div>
-        <ol>{/* TBD */}</ol>
+        <ol className="border border-gray-400 p-1 text-lg">{moves}</ol>
       </div>
     </div>
   );
