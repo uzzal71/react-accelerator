4adfb87fb402f919c475309f60cc659872cbf462
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-13 12:50:49 +0600

    version 1.11

--- 1.11/src/App.jsx
+++ 1.11/src/App.jsx
@@ -1,9 +1,17 @@
-import People from "./components/People";
+import Recipe from "./components/Recipe";
+import TeaSet from "./components/TeaSet";
 
 function App() {
   return (
     <div>
-      <People />
+      <section>
+        <h1>Spiced Chai Recipe</h1>
+        <h2>For two</h2>
+        <Recipe drinkers={2} />
+        <h2>For a gathering</h2>
+        <Recipe drinkers={4} />
+      </section>
+      <TeaSet />
     </div>
   );
 }
---
+++ 1.11/src/components/TeaSet.jsx
@@ -0,0 +1,11 @@
+import Cup from "./Cup";
+
+export default function TeaSet() {
+  return (
+    <>
+      <Cup guest={1} />
+      <Cup guest={2} />
+      <Cup guest={3} />
+    </>
+  );
+}
