aebb0e781453cd80f7a7f62505d3c7b794680db8
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-14 19:48:51 +0600

    version 2.7

--- 2.7/src/components/Counter.jsx
+++ 2.7/src/components/Counter.jsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
 
 export default function Counter() {
-  const [number, setNumber] = useState(1);
+  const [number, setNumber] = useState(0);
 
   return (
     <>
       <h1>{number}</h1>
       <button
         onClick={() => {
-          setNumber(number + 1);
-          setNumber(number + 1);
-          setNumber(number + 1);
+          // Use the functional form to get the latest state
+          setNumber((prevNumber) => prevNumber + 1);
+          setNumber((prevNumber) => prevNumber + 1);
+          setNumber((prevNumber) => prevNumber + 1);
         }}
       >
         +3
