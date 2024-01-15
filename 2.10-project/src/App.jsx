f9c71c67da07be19e2b8a90720608b9cf83319ef
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-16 00:22:33 +0600

    verstion 2.10-project

--- 2.10-project/src/App.jsx
+++ 2.10-project/src/App.jsx
@@ -1,7 +1,7 @@
 import Footer from "./Footer";
 import Header from "./Header";
 import HeroSection from "./HeroSection";
-import TaskBoard from "./tasks/TaskBoard";
+import TaskBoard from "./task/TaskBoard";
 
 function App() {
   return (
---
+++ 2.10-project/src/task/TaskBoard.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import NoTasksFound from "./NoTasksFound";
+import SearchTask from "./SearchTask";
+import TaskAction from "./TaskAction";
+import TaskList from "./TaskList";
+
+export default function TaskBoard() {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learn React Native",
+    description:
+      "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
+    tags: ["web", "react", "js"],
+    priority: "High",
+    isFavorite: true,
+  };
+  // eslint-disable-next-line no-unused-vars
+  const [tasks, setTasks] = useState([defaultTask]);
+
+  return (
+    <section className="mb-20" id="tasks">
+      <div className="container">
+        <div className="p-2 flex justify-end">
+          <SearchTask />
+        </div>
+        <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+          <TaskAction />
+          {tasks.length > 0 ? <TaskList tasks={tasks} /> : <NoTasksFound />}
+        </div>
+      </div>
+    </section>
+  );
+}
