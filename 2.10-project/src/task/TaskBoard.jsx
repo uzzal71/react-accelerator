7eb2ea2a6ffb3a9bd27b9efd66f6add31a00ad56
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-16 20:26:11 +0600

    verstion 2.10-project

--- 2.10-project/src/task/TaskBoard.jsx
+++ 2.10-project/src/task/TaskBoard.jsx
@@ -43,7 +43,7 @@ export default function TaskBoard() {
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskAction
             onAddClick={handleAddTask}
-            removeTask={handleRemoveAllTask}
+            onRemovedTask={handleRemoveAllTask}
           />
           {tasks.length > 0 ? <TaskList tasks={tasks} /> : <NoTasksFound />}
         </div>
