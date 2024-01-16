2c0d8a2462f58721f95a17cb08889d331652732e
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-16 20:44:17 +0600

    verstion 2.10-project

--- 2.10-project/src/task/TaskBoard.jsx
+++ 2.10-project/src/task/TaskBoard.jsx
@@ -19,21 +19,20 @@ export default function TaskBoard() {
   const [tasks, setTasks] = useState([defaultTask]);
   const [showAddModal, setShowAddModel] = useState(false);
 
-  function handleAddTask() {
-    /*
-    const newTask = {
-      id: crypto.randomUUID(),
-      title: "Learn Go Language",
-      description:
-        "I want to Learn Go Language Nat such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "go", "postgres"],
-      priority: "High",
-      isFavorite: false,
-    };
-    setTasks([...tasks, newTask]);
-    */
-    setShowAddModel(true);
-  }
+  // function handleAddTask() {
+  //   const newTask = {
+  //     id: crypto.randomUUID(),
+  //     title: "Learn Go Language",
+  //     description:
+  //       "I want to Learn Go Language Nat such thanI can treat it like my slave and make it do whatever I want to do.",
+  //     tags: ["web", "go", "postgres"],
+  //     priority: "High",
+  //     isFavorite: false,
+  //   };
+  //   setTasks([...tasks, newTask]);
+  //   */
+  //   setShowAddModel(true);
+  // }
 
   function handleRemoveAllTask() {
     setTasks([]);
@@ -58,7 +57,7 @@ export default function TaskBoard() {
         </div>
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskAction
-            onAddClick={handleAddTask}
+            onAddClick={() => setShowAddModel(true)}
             onRemovedTask={handleRemoveAllTask}
           />
           {tasks.length > 0 ? <TaskList tasks={tasks} /> : <NoTasksFound />}
