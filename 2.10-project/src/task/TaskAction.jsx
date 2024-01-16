93cdce562fb4ab13e6462f51df6308afe1250b85
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-16 20:25:31 +0600

    verstion 2.10-project

--- 2.10-project/src/task/TaskAction.jsx
+++ 2.10-project/src/task/TaskAction.jsx
@@ -1,10 +1,10 @@
-export default function TaskAction({ addTask, removeTask }) {
+export default function TaskAction({ onAddClick, removeTask }) {
   return (
     <div className="mb-14 items-center justify-between sm:flex">
       <h2 className="text-2xl font-semibold max-sm:mb-4">Your Tasks</h2>
       <div className="flex items-center space-x-5">
         <button
-          onClick={addTask}
+          onClick={onAddClick}
           className="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold"
         >
           Add Task
