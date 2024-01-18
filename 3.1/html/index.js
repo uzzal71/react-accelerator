9bd3ef43b94199dc2a59bcfdd7a20c72daa42dec
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-19 00:46:06 +0600

    version 3.1

--- 3.1/html/index.js
+++ 3.1/html/index.js
@@ -46,7 +46,7 @@ function submitForm(answer) {
   // Pretend it's hitting the network.
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (answer.toLowerCase() === "istanbul") {
+      if (answer.toLowerCase() === "dhaka") {
         resolve();
       } else {
         reject(new Error("Good guess but a wrong answer. Try again!"));
