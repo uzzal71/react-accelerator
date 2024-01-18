12c7ef895300beab44c5b83aafc5f1a3de74d548
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-18 23:49:49 +0600

    Assignment-02

--- 2.1/src/App.jsx
+++ 2.1/src/App.jsx
@@ -1,21 +1,13 @@
 export default function App() {
   return (
-    <Toolbar
-      onPlayMovie={() => alert("Playing!")}
-      onUploadImage={() => alert("Uploading!")}
-    />
-  );
-}
-
-function Toolbar({ onPlayMovie, onUploadImage }) {
-  return (
-    <div>
-      <Button onClick={onPlayMovie}>Play Movie</Button>
-      <Button onClick={onUploadImage}>Upload Image</Button>
+    <div
+      className="Toolbar"
+      onClick={() => {
+        alert("You clicked on the toolbar!");
+      }}
+    >
+      <button onClick={() => alert("Playing!")}>Play Movie</button>
+      <button onClick={() => alert("Uploading!")}>Upload Image</button>
     </div>
   );
-}
-
-function Button({ onClick, children }) {
-  return <button onClick={onClick}>{children}</button>;
 }
