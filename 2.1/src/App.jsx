ea092af51691539f5f48bcad4107ccd512f717ff
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-18 23:47:33 +0600

    Assignment-02

--- 2.1/src/App.jsx
+++ 2.1/src/App.jsx
@@ -1,12 +1,21 @@
-function Button({ onSmash, children }) {
-  return <button onClick={onSmash}>{children}</button>;
-}
-
 export default function App() {
   return (
+    <Toolbar
+      onPlayMovie={() => alert("Playing!")}
+      onUploadImage={() => alert("Uploading!")}
+    />
+  );
+}
+
+function Toolbar({ onPlayMovie, onUploadImage }) {
+  return (
     <div>
-      <Button onSmash={() => alert("Playing!")}>Play Movie</Button>
-      <Button onSmash={() => alert("Uploading!")}>Upload Image</Button>
+      <Button onClick={onPlayMovie}>Play Movie</Button>
+      <Button onClick={onUploadImage}>Upload Image</Button>
     </div>
   );
+}
+
+function Button({ onClick, children }) {
+  return <button onClick={onClick}>{children}</button>;
 }
