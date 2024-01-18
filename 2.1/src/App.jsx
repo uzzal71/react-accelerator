3e9979ef5d08d8f22337493e7c96713214486945
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-19 00:03:20 +0600

    Assignment-02

--- 2.1/src/App.jsx
+++ 2.1/src/App.jsx
@@ -1,26 +1,13 @@
-function Button({ onClick, children }) {
-  return (
-    <button
-      onClick={(e) => {
-        e.stopPropagation();
-        onClick();
-      }}
-    >
-      {children}
-    </button>
-  );
-}
-
 export default function App() {
   return (
-    <div
-      className="Toolbar"
-      onClick={() => {
-        alert("You clicked on the toolbar!");
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        alert("Submitting!");
       }}
     >
-      <Button onClick={() => alert("Playing!")}>Play Movie</Button>
-      <Button onClick={() => alert("Uploading!")}>Upload Image</Button>
-    </div>
+      <input />
+      <button>Send</button>
+    </form>
   );
 }
