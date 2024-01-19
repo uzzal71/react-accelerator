8116f949a6a27cbc6ddb3b6aa9f13a754655e2a9
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-19 20:56:06 +0600

    version 3.2

---
+++ 3.2/src/components/Form.jsx
@@ -0,0 +1,14 @@
+export default function Form() {
+  return (
+    <>
+      <form>
+        <textarea></textarea>
+        <br />
+        <button>Submit</button>
+        <p>Loading...</p>
+        <p className="Error">There was an error</p>
+      </form>
+      <h1>Thats right!</h1>
+    </>
+  );
+}
