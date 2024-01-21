23f0fbe612b1cfff71959cc0038ed822938eaf8f
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-21 19:39:46 +0600

    version 3.10

--- 3.10/src/App.jsx
+++ 3.10/src/App.jsx
@@ -1,12 +1,23 @@
 import Counter from "./components/Counter";
 
-function App() {
+import { useState } from "react";
+
+export default function App() {
+  const [showB, setShowB] = useState(true);
   return (
     <div>
       <Counter />
-      <Counter />
+      {showB && <Counter />}
+      <label>
+        <input
+          type="checkbox"
+          checked={showB}
+          onChange={(e) => {
+            setShowB(e.target.checked);
+          }}
+        />
+        Render the second counter
+      </label>
     </div>
   );
 }
-
-export default App;
