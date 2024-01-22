2a0b9ede8c56ce50125d76e96b9919cd17b6bf78
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-22 21:22:44 +0600

    version 3.12

--- 3.12/src/App.jsx
+++ 3.12/src/App.jsx
@@ -1,30 +1,7 @@
-import { useState } from "react";
-import Counter from "./components/Counter";
-
 export default function App() {
-  const [isFancy, setIsFancy] = useState(false);
-
   return (
     <div>
-      {isFancy ? (
-        <div>
-          <Counter isFancy={true} />
-        </div>
-      ) : (
-        <div>
-          <Counter isFancy={false} />
-        </div>
-      )}
-      <label>
-        <input
-          type="checkbox"
-          checked={isFancy}
-          onChange={(e) => {
-            setIsFancy(e.target.checked);
-          }}
-        />
-        Use fancy styling
-      </label>
+      <div>Hello world</div>
     </div>
   );
 }
