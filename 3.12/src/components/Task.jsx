8bd05e66d8e0e5d5a0cd030c007153d3f3f5be8e
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-22 23:25:05 +0600

    version 3.12

--- 3.12/src/components/Task.jsx
+++ 3.12/src/components/Task.jsx
@@ -3,12 +3,28 @@ import { useState } from "react";
 export default function Task({ task }) {
   const [isEditing, setIsEditing] = useState(false);
 
+  let taskContent;
+  if (isEditing) {
+    taskContent = (
+      <>
+        <input type="text" value={task.text} />
+        <button>Save</button>
+      </>
+    );
+  } else {
+    taskContent = (
+      <>
+        {task.text}
+        <button>Edit</button>
+      </>
+    );
+  }
+
   return (
     <li>
       <label>
         <input type="checkbox" />
-        {isEditing ? <input type="text" value={task.text} /> : task.text}
-        <button>{isEditing ? "Save" : "Edit"}</button>
+        {taskContent}
         <button>Delete</button>
       </label>
     </li>
