55cb8ee68ecc564ba30dbed9684ac6737a72dfc2
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-23 22:51:04 +0600

    version 3.16

--- 3.16/src/components/Section.jsx
+++ 3.16/src/components/Section.jsx
@@ -1,3 +1,9 @@
-export default function Section({ children }) {
-  return <section className="section">{children}</section>;
+import { LevelContext } from "../contexts/LevelContext.js";
+
+export default function Section({ level, children }) {
+  return (
+    <section className="section">
+      <LevelContext.Provider value={level}>{children}</LevelContext.Provider>
+    </section>
+  );
 }
