b8149ce31477a6c8741349bd1e2e68ea11a38fbf
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-24 00:39:43 +0600

    version 3.19

---
+++ 3.19/src/App.jsx
@@ -0,0 +1,14 @@
+import AllPosts from "./components/AllPosts";
+import Heading from "./components/Heading";
+import Post from "./components/Post";
+import Section from "./components/Section";
+
+export default function App() {
+  return (
+    <Section>
+      <Heading>My Profile</Heading>
+      <Post title="Hello traveller!" body="Read about my adventures." />
+      <AllPosts />
+    </Section>
+  );
+}
---
+++ 3.19/src/handler.js
@@ -0,0 +1,13 @@
+// setTasks([...tasks, { id: getNextId(tasks), text: text, done: false }]);
+
+/**
+ const nextTask = tasks.map((task) => {
+      if (task.id === newTask.id) {
+        return newTask;
+      }
+      return task;
+    });
+    setTasks(nextTask);
+ */
+
+// setTasks(tasks.filter((task) => task.id !== taskId));
