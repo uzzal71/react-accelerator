bb83887f1f9acbff4500ef2742e9a58319c49dae
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-25 21:19:18 +0600

    version 3.21-project

--- 3.21-project/src/App.jsx
+++ 3.21-project/src/App.jsx
@@ -1,9 +1,15 @@
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar />
+        </div>
+      </main>
     </>
   );
 }
