b4b378758fb7b23ca33ae510ab232260f23627a0
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-26 00:30:44 +0600

    version 3.21-project

--- 3.21-project/src/App.jsx
+++ 3.21-project/src/App.jsx
@@ -2,18 +2,21 @@ import Footer from "./Footer";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import MovieList from "./cine/MovieList";
+import { MovieContext } from "./context";
 
 function App() {
   return (
     <>
-      <Header />
-      <main>
-        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
-          <Sidebar />
-          <MovieList />
-        </div>
-      </main>
-      <Footer />
+      <MovieContext.Provider value={null}>
+        <Header />
+        <main>
+          <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+            <Sidebar />
+            <MovieList />
+          </div>
+        </main>
+        <Footer />
+      </MovieContext.Provider>
     </>
   );
 }
