64d4813f0f4391de50a8e5250e286daf500c89d5
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-27 01:03:56 +0600

    version 3.22-assignment

--- 3.22-assignment/src/task/TaskAction.jsx
+++ 3.22-assignment/src/task/TaskAction.jsx
@@ -1,7 +1,10 @@
-export default function TaskAction() {
+export default function TaskAction({ onAddClick }) {
   return (
     <>
-      <button className="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold">
+      <button
+        onClick={onAddClick}
+        className="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold"
+      >
         Add Task
       </button>
       <button className="rounded-md bg-red-500 px-3.5 py-2.5 text-sm font-semibold">
--- 3.22-assignment/src/task/TaskBoard.jsx
+++ 3.22-assignment/src/task/TaskBoard.jsx
@@ -1,17 +1,26 @@
+import { useState } from "react";
+import AddTaskModal from "./AddTaskModal";
 import SearchTask from "./SearchTask";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
 
 export default function TaskBoard() {
+  const [showAddModal, setShowAddModal] = useState(false);
+
+  const handleCloseClick = () => {
+    setShowAddModal(false);
+  };
+
   return (
     <section className="mb-20" id="tasks">
+      {showAddModal && <AddTaskModal onCloseClick={handleCloseClick} />}
       <div className="container">
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <div className="mb-14 items-center justify-between sm:flex">
             <h2 className="text-2xl font-semibold max-sm:mb-4">Your Tasks</h2>
             <div className="flex items-center space-x-5">
               <SearchTask />
-              <TaskAction />
+              <TaskAction onAddClick={() => setShowAddModal(true)} />
             </div>
           </div>
           <TaskList />
