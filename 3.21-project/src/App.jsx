31850c4a6fd396da1120196de6076ce82d0eb16d
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-26 20:33:38 +0600

    version 3.21-project

--- 3.21-project/src/App.jsx
+++ 3.21-project/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-      <MovieContext.Provider value={null}>
+      <MovieContext.Provider value={{ cartData, setCartData }}>
         <Header />
         <main>
           <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
