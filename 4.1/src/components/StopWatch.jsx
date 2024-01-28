e437c1e49f00096995113bfc89e66f94453e1b76
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-28 19:41:59 +0600

    version 4.1

--- 4.1/src/components/StopWatch.jsx
+++ 4.1/src/components/StopWatch.jsx
@@ -1,8 +1,34 @@
+import { useRef, useState } from "react";
+
 export default function StopWatch() {
+  const [startTime, setStartTime] = useState(null);
+  const [now, setNow] = useState(null);
+  const intervalRef = useRef(null);
+
+  const handleStart = () => {
+    setStartTime(Date.now());
+    setNow(Date.now());
+
+    clearInterval(intervalRef.current);
+    intervalRef.current = setInterval(() => {
+      setNow(Date.now());
+    }, 10);
+  };
+
+  function handleStop() {
+    clearInterval(intervalRef.current);
+  }
+
+  let secondsPassed = 0;
+  if (startTime != null && now != null) {
+    secondsPassed = (now - startTime) / 1000;
+  }
+
   return (
     <>
-      <h1>Time passed:</h1>
-      <button>Start</button>
+      <h1>Time passed: {secondsPassed}</h1>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
     </>
   );
 }
