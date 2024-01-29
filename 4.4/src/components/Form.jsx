638b680425a8ec61c87369fcefc8199c38a54375
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-29 22:23:43 +0600

    version 4.4

--- 4.4/src/components/Form.jsx
+++ 4.4/src/components/Form.jsx
@@ -5,9 +5,7 @@ export default function Form() {
   const inputRef = useRef(null);
 
   function handleClick() {
-    console.log(inputRef.current);
     inputRef.current.focus();
-    inputRef.current.style.borderRadius = "10px";
   }
 
   return (
