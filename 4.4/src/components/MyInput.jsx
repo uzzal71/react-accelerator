dcada7af1073ef955f79d555a27007a59d0bb7f8
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-29 22:27:10 +0600

    version 4.4

--- 4.4/src/components/MyInput.jsx
+++ 4.4/src/components/MyInput.jsx
@@ -1,9 +1,15 @@
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const MyInput = (props, ref) => {
+  const realInputRef = useRef(null);
+
+  useImperativeHandle(ref, () => ({
+    focus: () => realInputRef.current.focus(),
+  }));
+
   return (
     <>
-      <input {...props} ref={ref} />
+      <input {...props} ref={realInputRef} />
     </>
   );
 };
