204dc1e365ff82d44fe6d4c7455da90761328ff9
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-29 23:42:21 +0600

    version 4.6

---
+++ 4.6/src/components/Form.jsx
@@ -0,0 +1,19 @@
+import { useRef } from "react";
+import MyInput from "./MyInput";
+
+export default function Form() {
+  const inputRef = useRef(null);
+
+  function handleClick() {
+    inputRef.current.focus();
+  }
+
+  return (
+    <>
+      <MyInput type="text" placeholder="Enter name" ref={inputRef} />
+      <br />
+      <br />
+      <button onClick={handleClick}>Focus the input</button>
+    </>
+  );
+}
