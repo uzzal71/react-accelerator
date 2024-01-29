cba95abd13b7eb7c3915a31901592507994d8a71
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-29 20:22:40 +0600

    version 4.3

--- 4.3/src/components/MyInput.jsx
+++ 4.3/src/components/MyInput.jsx
@@ -1,7 +1,13 @@
-export default function MyInput(props) {
+import { forwardRef } from "react";
+
+const MyInput = (props) => {
   return (
     <>
       <input {...props} />
     </>
   );
-}
+};
+
+const ForwardedMyInput = forwardRef(MyInput);
+
+export default ForwardedMyInput;
