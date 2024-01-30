474f50a8ac550fd9a51ac8fa52ecba9057cefec8
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-31 00:10:35 +0600

    version 4.9

--- 4.9/src/App.jsx
+++ 4.9/src/App.jsx
@@ -1,17 +1,7 @@
-import { useState } from "react";
-import ChatRoom from "./components/ChatRoom";
-
 function App() {
-  const [show, setShow] = useState(true);
-
   return (
     <>
-      <div>
-        <button onClick={() => setShow((s) => !s)}>
-          {show ? "Hide" : "Show"}
-        </button>
-      </div>
-      {show && <ChatRoom />}
+      <div>Hello world</div>
     </>
   );
 }
