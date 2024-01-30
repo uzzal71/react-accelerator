c11e9be4747bcc9d2e53b96a7abe34b71d10830b
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-31 00:54:06 +0600

    version 4.10

---
+++ 4.10/src/App.jsx
@@ -0,0 +1,21 @@
+import Animation from "./components/Animation";
+
+function App() {
+  return (
+    <div>
+      {/* example 1 */}
+      {/* <Map /> */}
+
+      {/* example 2 */}
+      {/* <Dialogue /> */}
+
+      {/* example 3 */}
+      {/* <Scroller /> */}
+
+      {/* example 4 */}
+      <Animation />
+    </div>
+  );
+}
+
+export default App;
