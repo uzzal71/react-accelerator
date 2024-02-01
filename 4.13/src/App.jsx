3839ca594e2b75a2bc214e1c496aaa3e6f95d630
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-01 20:27:42 +0600

    version 4.13

---
+++ 4.13/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+// import Example4 from "./components/Example4";
+// import Example5 from "./components/Example5";
+import Example6 from "./components/Example6";
+
+export default function App() {
+  return (
+    <div>
+      {/* <Example4 /> */}
+      {/* <Example5 /> */}
+      <Example6 />
+    </div>
+  );
+}
