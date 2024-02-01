460d7666713efff44b96c6017acbc68d2be4eb3b
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-01 19:42:49 +0600

    version 4.12

---
+++ 4.12/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import Playground from "./components/Playground";
+
+export default function App() {
+  const [show, setShow] = useState(false);
+
+  return (
+    <>
+      <button onClick={() => setShow(!show)}>
+        {show ? "Unmount" : "Mount"} the component
+      </button>
+      {show && <hr />}
+      {show && <Playground />}
+    </>
+  );
+}
