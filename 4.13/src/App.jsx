f66f23b0ded2dca87741beece94e5d8df8fdcd5b
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-01 20:28:28 +0600

    version 4.13

--- 4.13/src/App.jsx
+++ 4.13/src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
-// import Example4 from "./components/Example4";
+import Example4 from "./components/Example4";
 // import Example5 from "./components/Example5";
-import Example6 from "./components/Example6";
+// import Example6 from "./components/Example6";
 
 export default function App() {
   return (
     <div>
-      {/* <Example4 /> */}
+      <Example4 />
       {/* <Example5 /> */}
-      <Example6 />
+      {/* <Example6 /> */}
     </div>
   );
 }
