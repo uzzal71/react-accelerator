6783b244c45fc052dc76ed45c55507917c8707b2
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-11 18:41:27 +0600

    version 4.16

--- 4.16/src/App.jsx
+++ 4.16/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import ChatRoom from "./components/ChatRoom";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [roomId, setRoomId] = useState("general");
+  const [showChat, setShowChat] = useState(true);
+  const [serverUrl, setServerUrl] = useState("https://localhost:1234");
+
+  const handleRoomChange = (e) => {
+    setRoomId(e.target.value);
+  };
 
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input
+          type="text"
+          value={serverUrl}
+          onChange={(e) => setServerUrl(e.target.value)}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div>
+        <button onClick={() => setShowChat((s) => !s)}>
+          {showChat ? "Hide Chat Room" : "Show Chat Room"}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      {showChat && (
+        <>
+          <hr />
+          <div>
+            Select Chat Room:{" "}
+            <select onChange={handleRoomChange}>
+              <option value="general">General</option>
+              <option value="travel">Travel</option>
+              <option value="music">Music</option>
+            </select>
+          </div>
 
-export default App
+          <ChatRoom roomId={roomId} serverUrl={serverUrl} />
+        </>
+      )}
+    </div>
+  );
+}
