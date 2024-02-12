fe3293732d5c918c215a37c262b6a147b6f46ba6
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-13 00:30:14 +0600

    version 4.28

---
+++ 4.28/src/App.jsx
@@ -0,0 +1,41 @@
+import { useCallback, useMemo, useState } from "react";
+import Button from "./components/Button";
+import ShowCount from "./components/ShowCount";
+import Title from "./components/Title";
+
+function App() {
+    const [count1, setCount1] = useState(0);
+    const [count2, setCount2] = useState(0);
+
+    const incrementByOne = useCallback(() => {
+        setCount1((prevCount) => prevCount + 1);
+    }, []);
+
+    const incrementByFive = useCallback(() => {
+        setCount2((prevCount) => prevCount + 5);
+    }, []);
+
+    const isEven = useMemo(() => {
+        let i = 0;
+        while (i < 1000000000) {
+            i += 1;
+        }
+
+        return count1 % 2 === 0;
+    }, [count1]);
+
+    return (
+        <div className="app">
+            <Title />
+            <ShowCount count={count1} title="Counter 1" />
+            <span>{isEven ? "Even" : "Odd"}</span>
+
+            <Button handleClick={incrementByOne}>Increment by one</Button>
+            <hr />
+            <ShowCount count={count2} title="Counter 2" />
+            <Button handleClick={incrementByFive}>Increment by five</Button>
+        </div>
+    );
+}
+
+export default App;
