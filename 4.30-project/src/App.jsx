295403c681480469e74487332ff117db4eee4cfd
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-15 00:07:32 +0600

    version 4.30

--- 4.30-project/src/App.jsx
+++ 4.30-project/src/App.jsx
@@ -1,11 +1,14 @@
 import Header from "./components/header/Header";
+import WeatherBoard from "./components/weather/WeatherBoard";
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <section></section>
+        <section>
+          <WeatherBoard />
+        </section>
       </main>
     </>
   );
