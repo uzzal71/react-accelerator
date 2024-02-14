14e16de8b16289a28a61aad41ebb69749a8b1546
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-14 23:22:05 +0600

    version 4.30

--- 4.30-project/src/components/header/Header.jsx
+++ 4.30-project/src/components/header/Header.jsx
@@ -1,14 +1,12 @@
-import Heart from "../assets/heart.svg";
-import Logo from "../assets/logo.svg";
-import Search from "../assets/search.svg";
+import Heart from "../../assets/heart.svg";
+import Search from "../../assets/search.svg";
+import Logo from "./Logo";
 
 export default function Header() {
   return (
     <header className="fixed w-full top-0 z-50 bg-gradient-to-b from-black/60 to-black/0 pb-10">
       <nav className="container flex items-center justify-between py-6">
-        <a href="./index.html">
-          <img className="h-9" src={Logo} alt="Weather App" />
-        </a>
+        <Logo />
 
         <div className="flex items-center gap-4 relative">
           <form action="#">
