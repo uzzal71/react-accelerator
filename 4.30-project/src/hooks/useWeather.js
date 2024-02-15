595f4cb22e35765cb0f76c95484518c5604be2d7
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-15 20:02:02 +0600

    version 4.30

--- 4.30-project/src/hooks/useWeather.js
+++ 4.30-project/src/hooks/useWeather.js
@@ -22,7 +22,38 @@ const useWeather = () => {
 
   const [error, setError] = useState(null);
 
-  const fetchWeatherData = async (latitude, longitude) => {};
+  const fetchWeatherData = async (latitude, longitude) => {
+    try {
+      setLoading({
+        ...loading,
+        state: true,
+        message: "Fetching weather data",
+      });
+      const apiKey = "656ba086bfaa96c2b8502189572097b8";
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
+      );
+
+      if (!response.ok) {
+        const errorMessage = `Fetching weather data failed: ${response.status}`;
+        throw new Error(errorMessage);
+      }
+
+      const data = await response.json();
+      const updatedWeatherData = {
+        ...weatherData,
+        location: data.name,
+      };
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading({
+        ...loading,
+        state: false,
+        message: "",
+      });
+    }
+  };
 };
 
 export default useWeather;
