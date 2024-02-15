1ee43c4129c66ff1bdc178b86665eb50d6080309
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-16 00:02:13 +0600

    version 4.30

--- 4.30-project/src/components/weather/WeatherCondition.jsx
+++ 4.30-project/src/components/weather/WeatherCondition.jsx
@@ -9,44 +9,53 @@ import { WeatherContext } from "../../context";
 function WeatherCondition() {
   const { weatherData } = useContext(WeatherContext);
 
+  const {
+    maxTemperature,
+    minTemperature,
+    humidity,
+    cloudPercentage,
+    wind,
+    climate,
+  } = weatherData;
+
   return (
     <div>
       <p className="text-sm lg:text-lg font-bold uppercase mb-8">
-        thunderstorm with light drizzle
+        The climate is <u>{climate}</u>
       </p>
       <ul className="space-y-6 lg:space-y-6">
         <li className="text-sm lg:text-lg flex items-center justify-between space-x-4">
           <span>Temp max</span>
           <div className="inline-flex space-x-4">
-            <p>{weatherData.maxTemperature}°</p>
+            <p>{Math.round(maxTemperature)}°</p>
             <img src={TempMaxIton} alt="temp-max" />
           </div>
         </li>
         <li className="text-sm lg:text-lg flex items-center justify-between space-x-4">
           <span>Temp min</span>
           <div className="inline-flex space-x-4">
-            <p>{weatherData.minTemperature}°</p>
+            <p>{Math.round(minTemperature)}°</p>
             <img src={TempMinIcon} alt="temp-min" />
           </div>
         </li>
         <li className="text-sm lg:text-lg flex items-center justify-between space-x-4">
           <span>Humadity</span>
           <div className="inline-flex space-x-4">
-            <p>{weatherData.humidity}%</p>
+            <p>{humidity}%</p>
             <img src={HumidityIcon} alt="humidity" />
           </div>
         </li>
         <li className="text-sm lg:text-lg flex items-center justify-between space-x-4">
           <span>Cloudy</span>
           <div className="inline-flex space-x-4">
-            <p>{weatherData.cloudPercentage}%</p>
+            <p>{cloudPercentage}%</p>
             <img src={CloudIcon} alt="cloudy" />
           </div>
         </li>
         <li className="text-sm lg:text-lg flex items-center justify-between space-x-4">
           <span>Wind</span>
           <div className="inline-flex space-x-4">
-            <p>{weatherData.wind}km/h</p>
+            <p>{wind}km/h</p>
             <img src={WindIcon} alt="wind" />
           </div>
         </li>
--- 4.30-project/src/hooks/index.js
+++ 4.30-project/src/hooks/index.js
@@ -1,3 +1,5 @@
+import useDebounce from "./useDebounce";
+import useLocalStorage from "./useLocalStorage";
 import useWeather from "./useWeather";
 
-export { useWeather };
+export { useDebounce, useLocalStorage, useWeather };
