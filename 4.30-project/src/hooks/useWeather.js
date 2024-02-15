1350f6216e777aa12a3b4f8a75b2381d09ee19b6
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-15 19:44:47 +0600

    version 4.30

--- 4.30-project/src/hooks/useWeather.js
+++ 4.30-project/src/hooks/useWeather.js
@@ -1,1 +1,17 @@
-const useWeather = () => {};
+import { useState } from "react";
+
+const useWeather = () => {
+  const [weatherData, setWeatherData] = useState({
+    location: "",
+    climate: "",
+    temperature: "",
+    maxTemperature: "",
+    minTemperature: "",
+    humidity: "",
+    cloudPercentage: "",
+    wind: "",
+    time: "",
+  });
+};
+
+export default useWeather;
