640aaa92fe93fabf308b89e4204684ec4cb3bb4e
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-16 20:03:17 +0600

    version 4.30

--- 4.30-project/src/components/header/Search.jsx
+++ 4.30-project/src/components/header/Search.jsx
@@ -1,6 +1,22 @@
+import { useContext } from "react";
 import SearchIcon from "../../assets/search.svg";
+import { LocationContext } from "../../context";
+import { getLocationByName } from "../../data/location-data";
+import { useDebounce } from "../../hooks";
 
-export default function Search() {
+function Search() {
+  const { setSelectedLocation } = useContext(LocationContext);
+
+  const doSearch = useDebounce((term) => {
+    const fetchedLocation = getLocationByName(term);
+    setSelectedLocation({ ...fetchedLocation });
+  }, 500);
+
+  function handleChange(e) {
+    const value = e.target.value;
+    doSearch(value);
+  }
+
   return (
     <form action="#">
       <div className="flex items-center space-x-2 py-2 px-3 group focus-within:bg-black/30 transition-all border-b border-white/50 focus-within:border-b-0 focus-within:rounded-md">
@@ -8,12 +24,15 @@ export default function Search() {
           className="bg-transparent  placeholder:text-white text-white w-full text-xs md:text-base outline-none border-none"
           type="search"
           placeholder="Search Location"
+          onChange={handleChange}
           required
         />
         <button type="submit">
-          <img src={SearchIcon} />
+          <img src={SearchIcon} alt="Search" />
         </button>
       </div>
     </form>
   );
 }
+
+export default Search;
--- 4.30-project/src/hooks/useWeather.js
+++ 4.30-project/src/hooks/useWeather.js
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { LocationContext } from "../context";
 
 const useWeather = () => {
   const [weatherData, setWeatherData] = useState({
@@ -20,6 +21,8 @@ const useWeather = () => {
   });
 
   const [error, setError] = useState(null);
+
+  const { selectedLocation } = useContext(LocationContext);
 
   const fetchWeatherData = async (latitude, longitude) => {
     try {
@@ -75,10 +78,14 @@ const useWeather = () => {
       message: "Finding location...",
     });
 
-    navigator.geolocation.getCurrentPosition(function (position) {
-      fetchWeatherData(position.coords.latitude, position.coords.longitude);
-    });
-  }, []);
+    if (selectedLocation.latitude && selectedLocation.longitude) {
+      fetchWeatherData(selectedLocation.latitude, selectedLocation.longitude);
+    } else {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        fetchWeatherData(position.coords.latitude, position.coords.longitude);
+      });
+    }
+  }, [selectedLocation.latitude, selectedLocation.longitude]);
 
   return {
     weatherData,
