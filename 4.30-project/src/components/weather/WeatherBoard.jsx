2c9cecd5b3c71a1b4244a918f2ba1d5af7d01efd
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-16 20:24:46 +0600

    version 4.30

--- 4.30-project/src/components/weather/WeatherBoard.jsx
+++ 4.30-project/src/components/weather/WeatherBoard.jsx
@@ -1,25 +1,15 @@
-import { useContext } from "react";
-import { WeatherContext } from "../../context";
 import AddToFavourite from "./AddToFavourite";
 import WeatherCondition from "./WeatherCondition";
 import WeatherHeadline from "./WeatherHeadline";
 
 function WeatherBoard() {
-  const { loading } = useContext(WeatherContext);
-
   return (
     <div className="container">
       <div className="grid bg-black/20 rounded-xl backdrop-blur-md border-2 lg:border-[3px] border-white/[14%] px-4 lg:px-14 py-6 lg:py-10 min-h-[520px] max-w-[1058px] mx-auto">
         <div className="grid md:grid-cols-2 gap-10 md:gap-6">
-          {loading.state ? (
-            <p>{loading.message}</p>
-          ) : (
-            <>
-              <AddToFavourite />
-              <WeatherHeadline />
-              <WeatherCondition />
-            </>
-          )}
+          <AddToFavourite />
+          <WeatherHeadline />
+          <WeatherCondition />
         </div>
       </div>
     </div>
