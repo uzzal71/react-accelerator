f9da19871766685b2f4473a590becdb7b559f80d
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-17 12:39:58 +0600

    version 5.1

--- 5.1/src/main.jsx
+++ 5.1/src/main.jsx
@@ -5,12 +5,14 @@ import Contact from "./Contact";
 import ErrorPage from "./ErrorPage";
 import Root from "./Root";
 import "./index.css";
+import { getContactsLoader } from "./loaders/contactsLoader";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: getContactsLoader,
     children: [
       {
         path: "contacts/:contactId",
