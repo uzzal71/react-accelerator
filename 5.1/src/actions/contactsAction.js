fc9162b906711dbede28d2c01de96af7a8303fb9
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-18 23:50:27 +0600

    version 5.1

--- 5.1/src/actions/contactsAction.js
+++ 5.1/src/actions/contactsAction.js
@@ -3,7 +3,7 @@ import { createContact, deleteContact, updateContact } from "../contacts";
 
 export async function createContactAction() {
   const contact = await createContact();
-  return { contact };
+  return redirect(`/contacts/${contact.id}/edit`);
 }
 
 export async function editContactAction({ request, params }) {
