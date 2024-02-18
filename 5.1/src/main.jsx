516c6234724e2ea65da676da2bfee5564d755e5b
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-18 23:43:48 +0600

    version 5.1

--- 5.1/src/main.jsx
+++ 5.1/src/main.jsx
@@ -4,6 +4,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Contact from "./Contact";
 import EditContact from "./EditContact";
 import ErrorPage from "./ErrorPage";
+import Index from "./Index";
 import Root from "./Root";
 import {
   createContactAction,
@@ -18,26 +19,37 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    errorElement: <ErrorPage />,
     loader: getContactsLoader,
     action: createContactAction,
+    errorElement: <ErrorPage />,
     children: [
       {
-        path: "contacts/:contactId",
-        element: <Contact />,
-        loader: getContactLoader,
-        action: updateContactFavorite,
-      },
-      {
-        path: "contacts/:contactId/edit",
-        element: <EditContact />,
-        loader: getContactLoader,
-        action: editContactAction,
-      },
-      {
-        path: "contacts/:contactId/destroy",
-        action: deleteContactAction,
-        errorElement: <div>Oops! There was an error.</div>,
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            index: true,
+            element: <Index />,
+          },
+          {
+            path: "/contacts/:contactId",
+            element: <Contact />,
+            loader: getContactLoader,
+            action: updateContactFavorite,
+          },
+          {
+            path: "/contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: getContactLoader,
+            action: editContactAction,
+          },
+          {
+            path: "/contacts/:contactId/destroy",
+            action: deleteContactAction,
+            errorElement: (
+              <div>Oops! There was an error deleting the contact!</div>
+            ),
+          },
+        ],
       },
     ],
   },
