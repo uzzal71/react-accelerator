de813e03ec10a47cef9be43c7192d78ef0caa673
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-18 23:13:58 +0600

    version 5.1

--- 5.1/src/loaders/contactsLoader.js
+++ 5.1/src/loaders/contactsLoader.js
@@ -4,7 +4,7 @@ export async function getContactsLoader({ request }) {
   const url = new URL(request.url);
   const q = url.searchParams.get("q");
   const contacts = await getContacts(q);
-  return { contacts };
+  return { contacts, q };
 }
 
 export async function getContactLoader({ params }) {
