54fcbcb2604d5a469222de344165495749569153
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-18 23:37:17 +0600

    version 5.1

--- 5.1/src/loaders/contactsLoader.js
+++ 5.1/src/loaders/contactsLoader.js
@@ -9,5 +9,11 @@ export async function getContactsLoader({ request }) {
 
 export async function getContactLoader({ params }) {
   const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("", {
+      status: 404,
+      statusText: "Not Found",
+    });
+  }
   return { contact };
 }
