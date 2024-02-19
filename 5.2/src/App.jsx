1ed144c67264a4dd1bdfc87d74b10adfc625b513
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-20 00:17:06 +0600

    version 5.2

--- 5.2/src/App.jsx
+++ 5.2/src/App.jsx
@@ -1,8 +1,14 @@
+import Posts from "./components/Posts";
+
 function App() {
   return (
     <div>
       <div>
         <h1>API Request Axios</h1>
+        <hr />
+        <div>
+          <Posts />
+        </div>
       </div>
     </div>
   );
