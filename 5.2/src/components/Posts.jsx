9c4533a8de0b435188379ff655f5025a2e5f1343
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-20 00:19:34 +0600

    version 5.2

--- 5.2/src/components/Posts.jsx
+++ 5.2/src/components/Posts.jsx
@@ -1,5 +1,21 @@
-function Posts() {
-  return <div>Posts</div>;
+export default function Posts({ posts, onDeletePost, onEditClick }) {
+  return (
+    <div>
+      <h2>All Posts</h2>
+      <div>
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>
+              <span>{post.id}</span>
+              <span>{post.title}</span>
+              <div>
+                <span onClick={() => onDeletePost(post.id)}>❌</span>
+                <span onClick={() => onEditClick(post)}>✏️</span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
-
-export default Posts;
