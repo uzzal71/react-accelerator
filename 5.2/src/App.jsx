bcdd6042a20a07f22b4d6b25b909a12b4e3b24e4
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-20 23:47:34 +0600

    version 5.2

--- 5.2/src/App.jsx
+++ 5.2/src/App.jsx
@@ -6,7 +6,53 @@ import Posts from "./components/Posts";
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [post, setPost] = useState(null);
   const [error, setError] = useState(null);
+
+  const handleAddPost = async (newPost) => {
+    try {
+      const id = posts.length ? Number(posts[posts.length - 1].id) + 1 : 1;
+
+      const finalPost = {
+        id: id.toString(),
+        ...newPost,
+      };
+
+      const response = await api.post("/posts", finalPost);
+
+      setPosts([...posts, response.data]);
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  const handleDeletePost = async (postId) => {
+    if (confirm("Are you sure you want to delete the post?")) {
+      try {
+        await api.delete(`/posts/${postId}`);
+        const newPosts = posts.filter((post) => post.id !== postId);
+        setPosts(newPosts);
+      } catch (err) {
+        setError(err.message);
+      }
+    } else {
+      console.log("You chose not to delete the post!");
+    }
+  };
+
+  const handleEditPost = async (updatedPost) => {
+    try {
+      const response = await api.patch(`/posts/${updatedPost.id}`, updatedPost);
+
+      const updatedPosts = posts.map((post) =>
+        post.id === response.data.id ? response.data : post
+      );
+
+      setPosts(updatedPosts);
+    } catch (err) {
+      setError(err.message);
+    }
+  };
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -30,7 +76,11 @@ function App() {
         <h1>API Request Axios</h1>
         <hr />
         <div>
-          <Posts posts={posts} onDeletePost={""} onEditClick={""} />
+          <Posts
+            posts={posts}
+            onDeletePost={handleDeletePost}
+            onEditClick={setPost}
+          />
           <hr />
           {!post ? (
             <AddPost onAddPost={handleAddPost} />
